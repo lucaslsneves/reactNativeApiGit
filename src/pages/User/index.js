94151e9a633772ec8c671a36ff05c76308dd81ed
lucasluz99 <lucasluzthatsme@gmail.com>
2019-11-26 21:08:18 -0300

    Add validate propTypes in User Page

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { View } from 'react-native';
 import api from '../../services/api';
 // import { Container } from './styles';
@@ -7,6 +8,12 @@ export default class User extends Component {
   static navigationOptions = ({ navigation }) => ({
     title: navigation.getParam('user').name,
   });
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      getParam: PropTypes.func,
+    }).isRequired,
+  };
 
   state = {
     stars: [],
